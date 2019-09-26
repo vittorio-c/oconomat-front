8e4d72733f59a26aa673fe282915e8f521dc0d17
reubenCOclock <chourakirgit@yahoo.com>
2019-09-26 11:27:38 +0200

    commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -42,9 +42,7 @@ const App = ({}) => (
       <Route  path ="/contact" component={Contact}/> 
       <Route path ="/recipe" component={Recipe}/>  
      
-       
-      
-      </div>
+    </div>
      
     
     </Router>
