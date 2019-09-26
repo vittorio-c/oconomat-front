4ef3860a8c995fe75c53970b3adb36159bc6f3e9
reubenCOclock <chourakirgit@yahoo.com>
2019-09-26 13:07:02 +0200

    premier foote

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './app.sass';
 
 const Footer =() =>{
-    return <div className="red"> Ceci est mon footer </div>
+    return <div class="container"> <div className="row"> 
+
+        <div className="greyBackground">
+            <div class="row">
+                <div class="col-xs-6 col-sm-6 col-lg-3"> Conditions Generales</div> 
+                <div class="col-xs-6 col-sm-6 col-lg-3"> Cookies</div>
+                <div class="col-xs-6 col-sm-6 col-lg-3"> Politique de Confidentialité</div>
+                <div class="col-xs-6 col-sm-6 col-lg-3"> Contact </div>
+            
+            </div>
+        
+        
+        </div>
+    </div> 
+    </div> 
 } 
 
 export default Footer;
