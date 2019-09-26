3c5f93002f30345a70cd9b6dd0aa18d103ac4b87
jerome mesic <jeromemesic@MacBook-Pro-de-jerome.local>
2019-09-26 21:27:56 +0200

    header with logo added in the navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import './Header.sass';
 const Header =() =>{
     return  <div className="Header">
         <nav className="navbar">
-            <a className="navbar-brand">Oconomat</a>
+            <a className="navbar-brand"><img src="src/ressources/pictures/logoOconomat.png" class="img-fluid" alt="Responsive image"></img></a>
                 <button type="button" class="btn btn-sm">Se connecter</button>
                 <button type="button" class="btn btn-sm">S'inscrire</button>
                 <button type="button" class="btn btn-sm">Contact</button>
