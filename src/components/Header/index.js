145a8d61da5d751fc876efed27b09c68a09c26f8
jerome mesic <jeromemesic@MacBook-Pro-de-jerome.local>
2019-09-27 14:57:54 +0200

    navbar toggle collapse full version

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,19 +3,44 @@ import './Header.sass';
 
 const Header =() =>{
     return  <div className="Header">
-        <nav className="navbar">
-            <a className="navbar-brand"><img src="src/ressources/pictures/logoOconomat.png" class="img-fluid" alt="Responsive image"></img></a>
-                <button type="button" class="btn btn-sm">Se connecter</button>
-                <button type="button" class="btn btn-sm">S'inscrire</button>
-                <button type="button" class="btn btn-sm">Contact</button>
+        <nav className="navbar navbar-expand-md navbar-light">
+            <a class="navbar-brand" href="#"><img src="src/ressources/pictures/logoOconomat.png" className="img-fluid" alt="Responsive image"></img></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Se connecter</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">S'inscrire</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mes objectifs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mes recettes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ma liste de course</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Deconnexion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
-    </div> 
-    
-        
+    </div>         
 } 
+
 /* informations from Bootstrap's documentation 
 https://getbootstrap.com/docs/4.3/components/buttons/
 */
+
 export default Header;
 
 
