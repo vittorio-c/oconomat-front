363c6c0f3244fdd54a61efb45099700388156758
jerome mesic <jeromemesic@MacBook-Pro-de-jerome.local>
2019-09-30 09:43:56 +0200

    last recipe version

--- src/components/HeaderBackToHomePage/index.js
+++ src/components/HeaderBackToHomePage/index.js
@@ -5,10 +5,10 @@ const Header =() =>{
     return  <div className="Header">
         <nav className="navbar navbar-expand-md navbar-light">
             <a class="navbar-brand" href="#"><img src="src/ressources/pictures/logoOconomat.png" className="img-fluid" alt="Responsive image"></img></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div className="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link" href="#">Accueil</a>
