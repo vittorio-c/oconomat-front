b950e5ac939ca4414f27f2ae7870e6c5eab11fdf
jerome mesic <jeromemesic@MacBook-Pro-de-jerome.local>
2019-09-30 13:52:54 +0200

    ContactRoute with href added

--- src/components/HeaderBackToHomePage/index.js
+++ src/components/HeaderBackToHomePage/index.js
@@ -4,14 +4,14 @@ import './HeaderBackToHomePage.sass';
 const HeaderBackToHomePage =() =>{
     return  <div className="Header">
         <nav className="navbar navbar-expand-md navbar-light">
-            <a className="navbar-brand" href="#"><img src="src/ressources/pictures/logoOconomat.png" className="img-fluid" alt="Responsive image"></img></a>
+            <a className="navbar-brand" href="/"><img src="src/ressources/pictures/logoOconomat.png" className="img-fluid" alt="Responsive image"></img></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Accueil</a>
+                        <a className="nav-link" href="/">Accueil</a>
                     </li>
                 </ul>
             </div>
