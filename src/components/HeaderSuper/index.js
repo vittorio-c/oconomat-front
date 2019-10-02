2ccf2389fa285aafc80ca76f86ccf893b92fa526
OllmannRobin <ollmann.robin@gmail.com>
2019-10-02 18:15:40 +0200

    Fixed header react-bootstrap

--- src/components/HeaderSuper/index.js
+++ src/components/HeaderSuper/index.js
@@ -11,26 +11,23 @@ const HeaderSuper =() =>{
         <Navbar.Brand href="/">Oconomat</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
+
             <Nav className="mr-auto">
-            <Nav.Link href="/">Accueil</Nav.Link>
-            <Nav.Link href="/account">Mon compte</Nav.Link>
-            <Nav.Link href="/objectives">Mes objectifs</Nav.Link>
-            <Nav.Link href="/recipes">Mes recettes</Nav.Link>
-            <Nav.Link href="/MarketList">Ma liste de course</Nav.Link>
-            <Nav.Link href="/recipe">Mes recettes</Nav.Link>
-            <Nav.Link href="/">Déconnexion</Nav.Link>
-            <Nav.Link href="/signin">Connexion</Nav.Link>
-            <Nav.Link href="/signup">S'inscrire</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-            
-            
-            
-            
+                <Nav.Link href="/">Accueil</Nav.Link>
+                <Nav.Link href="/account">Mon compte</Nav.Link>
+                <Nav.Link href="/objectives">Mes objectifs</Nav.Link>
+                <Nav.Link href="/recipes">Mes recettes</Nav.Link>
+                <Nav.Link href="/MarketList">Ma liste de course</Nav.Link>
+                <Nav.Link href="/recipe">Mes recettes</Nav.Link>
+                <Nav.Link href="/signup">S'inscrire</Nav.Link>
+                <Nav.Link href="/contact">Contact</Nav.Link>
+                
             </Nav>
-            <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-            </Form>
+            <div className ='float-right text-center'>
+                    <Nav.Link href="/"><Button className='btn btn-center' variant="outline-danger">Déconnexion</Button></Nav.Link>
+                    <Nav.Link href="/signin"><Button className='btn btn-center' variant="outline-success">Connexion</Button></Nav.Link>
+            </div>
+            
         </Navbar.Collapse>
         </Navbar>
     </div>         
