0a1ccf751ab44b5473c89d49eb3649fe4946d803
reubenCOclock <chourakirgit@yahoo.com>
2019-10-03 21:02:15 +0200

    inscription fait mais pas du beau code

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -3,38 +3,38 @@ import './SignUp.sass';
 
 import {connect} from 'react-redux';
 
-const InscriptionStatic =({testSubmit})=>{
+const InscriptionStatic =({insertInputFirstName,insertInputLastName,insertInputEmail,insertMDP,insertMDPConfirm,onFormSubmitManipulateArray,inputValueFirstName,inputValueLastName,inputValueEmail,inputValueMDP,inputValueConfirmMDP})=>{
   return (
     <body className ="Site">
           <div className="Site-content">
               <main className="main">
-                  <InscriptionStaticPage testSubmit={testSubmit}/>
+                  <InscriptionStaticPage insertInputFirstName={insertInputFirstName} insertInputLastName={insertInputLastName} insertInputEmail={insertInputEmail} insertMDP={insertMDP} insertMDPConfirm={insertMDPConfirm} onFormSubmitManipulateArray={()=>{onFormSubmitManipulateArray(inputValueFirstName,inputValueLastName,inputValueEmail,inputValueMDP,inputValueConfirmMDP, event)}}/>
               </main>
           </div>
       </body>
     )
 } 
 
-const InscriptionStaticPage =({testSubmit}) => {
+const InscriptionStaticPage =({insertInputFirstName,insertInputLastName,insertInputEmail,insertMDP,insertMDPConfirm,onFormSubmitManipulateArray}) => {
   return <div className ='food-background mt-3'>
-  <form className="d-flex flex-column" onSubmit={testSubmit}>
+  <form className="d-flex flex-column" onSubmit={onFormSubmitManipulateArray}>
   <div className="formBlock">
           
           <h2 className="inscription-title">Inscription</h2>
               <div className="form-group mt-4">
-                <input type="text" className="form-control rounded-left rounded-right" name="lastname" placeholder ='Your lastname'/>
+                <input type="text" onChange={insertInputLastName} className="form-control rounded-left rounded-right" name="lastname" placeholder ='Your lastname'/>
               </div> 
               <div className="form-group">
-                <input type="text" className="form-control rounded-left rounded-right" name="firstname" placeholder ='Your firstname'/>
+                <input type="text" onChange={insertInputFirstName}className="form-control rounded-left rounded-right" name="firstname" placeholder ='Your firstname'/>
               </div> 
               <div className="form-group">
-                <input type="email" className="form-control rounded-left rounded-right" name="email" placeholder ='Your email'/>
+                <input type="email" onChange={insertInputEmail} className="form-control rounded-left rounded-right" name="email" placeholder ='Your email'/>
               </div> 
               <div className="form-group">
-                <input type="password" className="form-control rounded-left rounded-right" name="email" placeholder ='Your password'/>
+                <input type="password" onChange={insertMDP} className="form-control rounded-left rounded-right" name="password" placeholder ='Your password'/>
               </div>
               <div className="form-group">
-                <input type="password" className="form-control rounded-left rounded-right mb-4" name="email" placeholder ='Confirm your password'/>
+                <input type="password" onChange={insertMDPConfirm} className="form-control rounded-left rounded-right mb-4" name="password-confirm" placeholder ='Confirm your password'/>
               </div>
               <button className="validation btn" type="submit">Envoyer</button>
               </div>
@@ -51,21 +51,63 @@ const InscriptionStaticPage =({testSubmit}) => {
 const connectionStrategies = connect(
     // 1er argument : stratégie de lecture (dans le state privé global)
     (state, ownProps) => { 
-      
+   
       return {
         letters: state.letters,
-        storedValues: state.storedValues
+        inputValues:state.inputValues,
+        storedValues: state.storedValues,
+        inputValueFirstName:state.inputValueFirstName,
+        inputValueLastName:state.inputValueLastName,
+        inputValueEmail:state.inputValueEmail,
+        inputValueMDP:state.inputValueMDP,
+        inputValueConfirmMDP:state.inputValueConfirmMDP,
+        newObj:state.newObj
+       
       };
     },
   
     // 2d argument : stratégie d'écriture (dans le state privé global)
     (dispatch, ownProps) => {
       return {
-       testSubmit:(event) =>{
-           event.preventDefault(); 
-           ownProps.history.push('/Objectives');
-                 
+       insertInputFirstName:(event) => { 
+         console.log('change-detected');
+          const action={type:'Insert-Input-FirstName',value:event.target.value}
+          dispatch(action)
        },
+       insertInputLastName :(event) => { 
+         console.log('change detected')
+         const action={type:'Insert-Input-LastName',value:event.target.value}
+         dispatch(action)
+       },
+
+       insertInputEmail: (event) => { 
+         console.log('change detected')
+         const action={type:'Insert-Input-Email',value:event.target.value} 
+         dispatch(action)
+       },
+
+       insertMDP:(event) => { 
+         console.log('change detected')
+         const action={type:'Insert-Input-MDP',value:event.target.value} 
+         dispatch(action)
+       },
+
+       insertMDPConfirm:(event) => { 
+        console.log('change detected')
+        const action={type:'Insert-Input-MDP-Confirm',value:event.target.value} 
+        dispatch(action)
+      },
+      onFormSubmitManipulateArray:(inputValueFirstName,inputValueLastName,inputValueEmail,inputValueMDP,inputValueConfirmMDP,event) => { 
+        event.preventDefault();
+        console.log([
+          inputValueFirstName,inputValueLastName,inputValueEmail,inputValueMDP,inputValueConfirmMDP
+        ])
+       
+        
+       
+      },
+     
+    
       };
     },
   );
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,19 +1,53 @@
 const initialState = {
   recipes:[],
-  recipe:[]
+  recipe:[],
+  inputValueFirstName:{},
+  inputValueLastName:{},
+  inputValueEmail:{},
+  inputValueMDP:{},
+  inputValueConfirmMDP:{},
+  inputValues: []
+  
 
 };
 
 const defaultAction = {};
 
 const reducer = (state = initialState, action = defaultAction) => {
-  switch (action.type) {
-    case 'UPDATE_INPUT_VALUE': {
-      return {
-        ...state,
-        recipes:action.value
-      }
+  switch (action.type) { 
+
+    case 'Insert-Input-FirstName':{
+      return {...state,inputValueFirstName:{firstname:action.value}}
+    }
+
+    case 'Insert-Input-LastName': {
+      return {...state,inputValueLastName:{lastname:action.value}}
+    }
+
+    case 'Insert-Input-Email': {
+      return {...state,inputValueEmail:{email:action.value}}
     } 
+
+    case 'Insert-Input-MDP': { 
+      return {...state,inputValueMDP:{password:action.value}}
+    } 
+
+    case 'Insert-Input-MDP-Confirm': {
+      return {...state,inputValueConfirmMDP:{passwordConfirm:action.value}}
+    }
+    case 'Submit-Form': { 
+      
+      return {...state,
+        inputValues:[state.inputValueFirstName,state.inputValueLastName,state.inputValueEmail,state.inputValueMDP,state.inputValueConfirmMDP]}
+        
+    } 
+    
+    case 'Get-Values' :{
+      return {
+        ...state.inputValues
+      }
+    }
+  
     
     case 'See-Recipe' :{
       return {
@@ -24,7 +58,9 @@ const reducer = (state = initialState, action = defaultAction) => {
     case 'Show-Recipes' : { 
       console.log('RECIPES FETCHED')
       return{...state,recipes:action.value} 
-    }
+    } 
+
+   
     default: {
       // return state;
       // Dans le cas où on ne comprend pas quelle est l'action à
