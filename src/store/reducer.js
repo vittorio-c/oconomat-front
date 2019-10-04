a0835dcd3498724078ac09fff4d69db8f2271bdc
OllmannRobin <ollmann.robin@gmail.com>
2019-10-04 16:22:10 +0200

    ConnexionBackFromDeath

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -65,8 +65,47 @@ const reducer = (state = initialState, action = defaultAction) => {
       console.log('RECIPES FETCHED')
       return{...state,recipes:action.value} 
     } 
-
-   
+    
+        case 'ENTER_PASSWORD' : {
+          console.log("password saisi")
+          return {
+            ...state,
+            passwordState:{password:action.value}
+          }
+        } 
+        case 'ENTER_EMAIL' : {
+          console.log("mot de passe saisi")
+          return {
+            ...state,
+            emailState:{email:action.value}
+          }
+        } 
+      case 'VALID_FORM' : {
+          console.log("Validation du formulaire")
+          return {
+            ...state,
+            passwordState:state.passwordState,
+            emailState:state.emailState
+          }
+        } 
+        case 'UPDATE_INPUT_VALUE': {
+          return {
+            ...state,
+            recipes:action.value
+          }
+        } 
+        
+        case 'See-Recipe' :{
+          return {
+            ...state,recipe:action.value
+          }
+        }
+    
+        case 'Show-Recipes' : { 
+          console.log('RECIPES FETCHED')
+          return{...state,recipes:action.value} 
+        }
+    
     default: {
       // return state;
       // Dans le cas où on ne comprend pas quelle est l'action à
