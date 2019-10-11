acc733cd463eb3c5bf8e835eac5b9bbad9102dd6
jerome mesic <jeromemesic@MacBook-Pro-de-jerome.local>
2019-10-11 11:24:03 +0200

    404 responsive

--- src/components/NoFound/index.js
+++ src/components/NoFound/index.js
@@ -13,9 +13,9 @@ const NoFound = () => {
                     4
                 </h1>
                 <h2 className="pt-4 pb-4 text-center font-weight-bolder text-warning">
-                Vous vous êtes perdu, ou n'avez tout simplement pas accès à cette page , pas d'inquiètude, il suffit de vous inscrire ou de vous connecter :</h2>
+                    Vous vous êtes perdu, ou n'avez tout simplement pas accès à cette page , pas d'inquiètude, il suffit de vous inscrire ou de vous connecter :</h2>
                 <p className = 'pt-4 pb-4 text-center font-weight-bolder text-warning display-2' >404</p>
-                <a type='button' href='/' className='btn btn-success btn-block'>Go Back</a>
+                <a type='button' href='/' className='btn-goback btn-success btn-block'>Go Back</a>
             </div>
         </div>
     )
