540f32b8f727ccb2809962fabd25e2063791d85f
OllmannRobin <ollmann.robin@gmail.com>
2019-10-13 12:52:29 +0200

    first commit

--- src/components/ForgottenPassword/index.js
+++ src/components/ForgottenPassword/index.js
@@ -1,77 +1,66 @@
 import React from 'react' 
 import axios from 'axios'
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import './ForgottenPassword.sass';
 
 const ForgottenPassword =({insertInputEmail,inputValueEmail,validRestart})=>{
+
   return (
     
           <div className="Site-content Site">
               <main className="main">
-                  <ForgottenPasswordStaticPage  insertInputEmail={insertInputEmail}  validRestart = {validRestart}/>
+              <div className ='food-background mt-3'>
+              <form className="d-flex flex-column"  >
+                  <div className="formBlock">
+                      <h2 className="inscription-title">Mot de passe oublié</h2>
+                      <div className="form-group mt-4">
+                          <div className="form-group">
+                          <input onChange={insertInputEmail} type="email" className="form-control rounded-left rounded-right" name="email" placeholder ='Votre email'/>
+                          </div> 
+                          <div className="information">
+                              <p>
+                                  Vous allez recevoir votre nouveau mot de passe.<br/>
+                                  N'oubliez pas de verifier vos spams.
+                              </p>
+                          </div>
+                          <button className="validation btn" onClick={()=>{validRestart(inputValueEmail,event)}}>Envoyer</button>
+                      </div>
+                   </div>
+              </form>      
+          </div>
+ 
               </main>
           </div>
       
     )
 } 
 
-const ForgottenPasswordStaticPage =({insertInputEmail,validRestart}) => {
-  return <div className ='food-background mt-3'>
-    <form className="d-flex flex-column" >
-        <div className="formBlock">
-            <h2 className="inscription-title">Mot de passe oublié</h2>
-            <div className="form-group mt-4">
-                <div className="form-group">
-                <input type="email" onChange={insertInputEmail} className="form-control rounded-left rounded-right" name="email" placeholder ='Votre email'/>
-                </div> 
-                <div className="information">
-                    <p>
-                        Vous allez recevoir votre nouveau mot de passe.<br/>
-                        N'oubliez pas de verifier vos spams.
-                    </p>
-                </div>
-                <button onClick={validRestart} className="validation btn" type="submit"><a href="/PasswordSend">Envoyer</a></button>
-            </div>
-         </div>
-    </form>      
-</div>
-}
-
-
-
-
-
-
 const connectionStrategies = connect(
     // 1er argument : stratégie de lecture (dans le state privé global)
-    (state, ownProps) => { 
-   
+    (state) => { 
       return {
         letters: state.letters,
         inputValues:state.inputValues,
         storedValues: state.storedValues,
         inputValueEmail:state.inputValueEmail,
-      };
+      }
     },
   
     // 2d argument : stratégie d'écriture (dans le state privé global)
-    (dispatch, ownProps) => {
+    (dispatch,inputValueEmail) => {
       return {
-       insertInputEmail: (event) => { 
-         console.log('change detected')
-         const action={type:'Insert-Input-Email',value:event.target.value} 
-         dispatch(action)
-       },
-      validRestart:(event) => { 
-        console.log('ma valeur d\'input'+inputValue)
+        insertInputEmail: (event) => { 
+          console.log('change detected')
+          const action={type:'Insert-Input-Email',value:event.target.value} 
+          dispatch(action)
+        },
+      validRestart:(inputValueEmail,event) => { 
         event.preventDefault();
-        console.log('ma valeur d\'input'+inputValue)
+        console.log('ma valeur d\'input'+inputValueEmail.email)
         var inputValues= inputValueEmail;
-       console.log('ma valeur d\'input'+inputValue)
         var stringifyInput=JSON.stringify(inputValues);
         var formData= new FormData();
-        console.log('hello world')
         formData.set('email',inputValueEmail.email);
        console.log('inputValueEmail.email'+formData)
 
@@ -95,6 +84,10 @@ const connectionStrategies = connect(
     },
   );
   
+  const ForgottenPasswordContainer = connectionStrategies(ForgottenPassword);
+  
   
 
-export default ForgottenPassword
+  
+
+export default ForgottenPasswordContainer
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -18,78 +18,85 @@ const HomePage = () => {
     return <div className ="bg-light">
     <Logo />
     <Steps />
-    <Explanations />
 </div>
 
 }
 
 const Logo = () => {
     return(
-    <div className ='food-image img-thumbnail text-light pt-4'>
-        <div className = "text-box">
-            <h2 className='home-catch-sentence m-3 '>Une panne d'inspiration ?</h2>
-            <h1 className='home-title'>OCONOMAT</h1>
-            <h3 className='home-catch-sentence m-3 '>vous propose des repas en respectant votre budget</h3>
+        <div className ='container text-center col-xs-12 col-lg-10'>
+            <div className ='food-image img-thumbnail text-light  pt-4'>
+                <div className = "text-box">
+                    <h2 className='home-catch-sentence m-3 '>Une panne d'inspiration ?</h2>
+                    <h1 className='home-title'>OCONOMAT</h1>
+                    <h3 className='home-catch-sentence m-3 '>vous propose des repas en respectant votre budget</h3>
+                </div>
+            </div>
         </div>
-    </div>
     )
 }
 
 const Steps = () => {
     return (
-        <div className = "mt-3 pt-3 mb-3 pb-3 steps container-fluid">
-            <table className="table table-striped">
-            <thead>
-            </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row" className = "text-warning font-weight-bold display-4">1</th>
-                        <th scope="col" className ="font-weight-bold">Oconomat vous propose des petits-déjeuners, des déjeuners et des dîners pendant 7 jours.</th>
-                    </tr>
-                    <tr>
-                        <th scope="row" className = "text-warning font-weight-bold display-4" >2</th>
-                        <td ><span className ="font-weight-bold">Faites vos courses.</span>
-                            <th className="font-italic">Oconomat vous fourni une liste de course déjà tout prête avec les ingredients dont vous aurez besoin pour la semaine.</th>
-                        </td>
-                    </tr>
-                    <tr>
-                    <th scope="row" className = "text-warning font-weight-bold display-4">3</th>
-                    <td className ="font-weight-bold"> Enfilez votre tablier et suivez les étapes de préparation.</td>
-                    </tr>
-                    <tr>
-                    <th scope="row" className = "text-warning font-weight-bold display-4">4</th>
-                    <td className ="font-weight-bold">Régalez-vous !</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <div>
+            <div className = "mt-3 pt-3 mb-3 pb-3 col-xs-12 ">
+                <table className="table table-striped">
+                <thead>
+                </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row" className = "text-warning font-weight-bold display-4 ">1</th>
+                            <th scope="col" className ="font-weight-bold">Oconomat vous propose des petits-déjeuners, des déjeuners et des dîners pendant 7 jours.</th>
+                        </tr>
+                        <tr>
+                            <th scope="row" className = "text-warning font-weight-bold display-4" >2</th>
+                            <td ><span className ="font-weight-bold">Faites vos courses.</span>
+                                <th className="font-italic">Oconomat vous fourni une liste de course déjà tout prête avec les ingredients dont vous aurez besoin pour la semaine.</th>
+                            </td>
+                        </tr>
+                        <tr>
+                        <th scope="row" className = "text-warning font-weight-bold display-4">3</th>
+                        <td className ="font-weight-bold"> Enfilez votre tablier et suivez les étapes de préparation.</td>
+                        </tr>
+                        <tr>
+                        <th scope="row" className = "text-warning font-weight-bold display-4">4</th>
+                        <td className ="font-weight-bold">Régalez-vous !</td>
+                        </tr>
+                    </tbody>
+                </table>
+            
+                <div >
+                    <h3 className= 'title-explaination text-center font-weight-bold '>Inscrivez vous via le formulaire</h3>
+                    <center><img src="src/ressources/pictures/inscription.png" className="exemple-img img-fluid img" alt="Responsive image"></img></center>
+                    <div></div>
+                </div>
+                <div >
+                    <h3 className= 'title-explaination text-center font-weight-bold mt-4'>Définissez le budget de la semaine dans la rubrique Mes objectifs</h3>
+                    <center><img src="src/ressources/pictures/objectives.png" className="exemple-img img-fluid img" alt="Responsive image"></img></center>
+                    
+                </div>  
+                <div >  
+                    <h3 className= 'title-explaination text-center font-weight-bold mt-4'>Decouvrez vos recettes</h3>
+                    <center><img src="src/ressources/pictures/recipe.png" className="exemple-img img-fluid img" alt="Responsive image"></img></center>
+                    </div>
+                <div >
+                    <h3 className= 'title-explaination text-center font-weight-bold mt-4'>Achetez vos ingredients en suivant votre liste de course</h3>
+                    <center><img src="src/ressources/pictures/list.png" className="exemple-img img-fluid img" alt="Responsive image"></img></center>
+                    </div>
+                <div >
+                    <h3 className= 'title-explaination text-center font-weight-bold mt-4'>
+                        C'est facile Pourquoi ne pas commencer maintenant ?
+                    </h3>
+                </div>
+
+                <div > 
+                    <div className="box">
+                    <a className ="start btn btn-success btn-md mt-3 center-block" href="/SignUp" role="button">Je commence maintenant</a>
+                    </div>
+                </div>
+                </div>
+            </div>
     )
 }
-
-const Explanations = () => {
-    return(
-        <div>
-            <h3 className= 'title-explaination text-center font-weight-bold'>Inscrivez vous via le formulaire</h3>
-            <center><img src="src/ressources/pictures/inscription.png" className="exemple-img img-fluid img" alt="Responsive image"></img></center>
-        
-            <h3 className= 'title-explaination text-center font-weight-bold mt-4'>Définissez le budget de la semaine dans la rubrique Mes objectifs</h3>
-            <center><img src="src/ressources/pictures/objectives.png" className="exemple-img img-fluid img" alt="Responsive image"></img></center>
-            
-            <h3 className= 'title-explaination text-center font-weight-bold mt-4'>Decouvrez vos recettes</h3>
-            <center><img src="src/ressources/pictures/recipe.png" className="exemple-img img-fluid img" alt="Responsive image"></img></center>
-   
-            <h3 className= 'title-explaination text-center font-weight-bold mt-4'>Achetez vos ingredients en suivant votre liste de course</h3>
-            <center><img src="src/ressources/pictures/list.png" className="exemple-img img-fluid img" alt="Responsive image"></img></center>
- 
-            <h3 className= 'title-explaination text-center font-weight-bold mt-4'>
-                C'est facile Pourquoi ne pas commencer maintenant ?
-            </h3>
-            <div className="box">
-            <a className ="start btn btn-success btn-md mt-3 center-block" href="/SignUp" role="button">Je commence maintenant</a>
-            </div>
-        </div>
-    )
-} 
-
 
 export default Home ;
