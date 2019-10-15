d7c114171bc4b70bc5ea0701812296a5284de569
Joffreycormont <j.cormont30@gmail.com>
2019-10-15 11:00:24 +0200

    redesign page contact + password forgotten

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -19,9 +19,8 @@ const InscriptionStatic =({insertInputFirstName,insertInputLastName,insertInputE
 
 const InscriptionStaticPage =({insertInputFirstName,insertInputLastName,insertInputEmail,insertMDP,insertMDPConfirm,onFormSubmitManipulateArray}) => {
   return <div className ='food-background mt-3'>
-  <form className="d-flex flex-column" onSubmit={onFormSubmitManipulateArray}>
-  <div className="formBlockSignUp">
-          
+  <form className="row justify-content-center" onSubmit={onFormSubmitManipulateArray}>
+  <div className="container formBlockSignUp">
           <h2 className="inscription-title-signUp">Inscription</h2>
               <div className="form-group mt-4">
                 <input type="text" onChange={insertInputLastName} className="form-control rounded-left rounded-right" name="lastname" placeholder ='Nom'/>
